d6659fd33cf9c3b28f9deae1ba816c7959dfbb86
MAURICIO HOYOS PADILLA <mahoyosp@fpanzenu.interno>
2017-03-17 08:41:15 -0500

    directiva ng-repeat parte 1

---
+++ Ejercicio_03/js/app.js
@@ -0,0 +1,17 @@
+(function(){
+
+var app = angular.module('universidadApp',[ ]);
+
+app.controller('listadoCtrl', ['$scope', function($scope){
+	$scope.listado=["Fernando Herrera","Melissa Florez", "Mauricio Hoyos","Erlis Alandete","Carlos Usme"];
+		
+
+
+
+}]);
+
+
+
+
+
+})();
