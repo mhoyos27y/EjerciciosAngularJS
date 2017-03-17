9c01b26aa042ee23f717e4e4f315e91f0ffca804
MAURICIO HOYOS PADILLA <mahoyosp@fpanzenu.interno>
2017-03-17 11:22:23 -0500

    nj-options

---
+++ Ejercicio_06/js/app.js
@@ -0,0 +1,22 @@
+(function(){
+
+var app = angular.module('ejemplosApp',[ ]);
+
+
+
+app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
+  
+  
+
+
+  
+
+
+
+}]);
+
+
+
+
+
+})();
