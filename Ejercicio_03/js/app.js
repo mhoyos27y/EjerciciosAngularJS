37b798ffb9a5d47bf560b63c046cfd26e0ee6c33
MAURICIO HOYOS PADILLA <mahoyosp@fpanzenu.interno>
2017-03-17 08:54:13 -0500

    directiva ng-repeat parte 2

--- Ejercicio_03/js/app.js
+++ Ejercicio_03/js/app.js
@@ -4,9 +4,24 @@ var app = angular.module('universidadApp',[ ]);
 
 app.controller('listadoCtrl', ['$scope', function($scope){
 	$scope.listado=["Fernando Herrera","Melissa Florez", "Mauricio Hoyos","Erlis Alandete","Carlos Usme"];
-		
+	
+	$scope.listadoProfesores={
+		profesores:[{
+			nombre:"Mauricio Hoyos",
+			edad: 27,
+			clase:"PEE"
+		},{
+			nombre:"Erlency Alandete",
+			edad: 24,
+			clase:"Ingenieria de Software"
+		},{
+			nombre:"Oscar Duvan Florez",
+			edad: 29,
+			clase:"DevOps"
+		}]
+	}
 
-
+	$scope.listaNumeros=[1,2,3,4,5,6,7,8,9,10];
 
 }]);
 
